ccd52675ba9de968284ad9716ed93cd6dc9ab297
Simon Lee <slee@epoch.com>
2016-02-28 00:41:23 -0800

    Add new soundcloud embed link

--- index.php
+++ index.php
@@ -727,8 +727,10 @@
 			<!-- row -->
 			<div class="row">
 				<h3>Latest mix from soundcloud</h3>
+				<p>Progressive House Mix on Feb 28, 2016</p>
+				<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/249277181&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
+				<p>Tech House Mix on Feb 27, 2016</p>
                 <iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/249206933&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
-                <p>Tech House Mix on Feb 27, 2016</p>
 			</div>
 			<!-- /row -->
 
